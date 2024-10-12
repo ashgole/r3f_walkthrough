09c28520d3f57192b873ecda9c26c790fc968f82
ashabb <ashishgole99955@gmail.com>
2024-10-12 18:56:53 +0530

    ring updated

--- app/components/experience/Experience.js
+++ app/components/experience/Experience.js
@@ -1,6 +1,6 @@
 import { ContactShadows, Environment, Lightformer, RandomizedLight, useGLTF } from '@react-three/drei';
-import { applyProps, useThree,} from '@react-three/fiber';
-import {  useMemo } from 'react';
+import { applyProps, useThree, } from '@react-three/fiber';
+import { useMemo, useState } from 'react';
 
 const CarModel = () => {
     const { scene, nodes, materials } = useGLTF('models/car.gltf')
@@ -19,6 +19,9 @@ const CarModel = () => {
 };
 
 const Experience = () => {
+    const { viewport } = useThree();
+    const ringScale = viewport.width < 7 ? 1.5 : 4;
+    const [isMobile, setisMobile] = useState(viewport.width < 7)
     // const cameraposition = [0, 5, -3.9]
     // const { cameraposition } = useControls({
     //     cameraposition: {
@@ -42,15 +45,29 @@ const Experience = () => {
         <>
             <CarModel />
             <hemisphereLight intensity={0.5} />
-            <ContactShadows resolution={1024} frames={1} position={[0, -1.16, 0]} scale={15} blur={0.5} opacity={1} far={20} />
-            <mesh scale={4} position={[3, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-                <ringGeometry args={[0.9, 1, 4, 1]} />
-                <meshStandardMaterial color="white" roughness={0.75} />
-            </mesh>
-            <mesh scale={4} position={[-3, 0, 0.2]} rotation={[-Math.PI / 2, 0, Math.PI / 2.0]}>
-                <ringGeometry args={[0.9, 1, 3, 1]} />
-                <meshStandardMaterial color="white" roughness={0.75} />
-            </mesh>
+            <ContactShadows resolution={1024} frames={1} position={[0, -0.5, 0.2]} scale={15} blur={0.5} opacity={1} far={20} />
+
+            {
+                isMobile ? <>
+                    <mesh scale={ringScale} position={[1, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+                        <ringGeometry args={[0.9, 1, 4, 1]} />
+                        <meshStandardMaterial color="white" roughness={0.75} />
+                    </mesh>
+                    <mesh scale={ringScale} position={[-1.5, 0, 0.2]} rotation={[-Math.PI / 2, 0, Math.PI / 2.0]}>
+                        <ringGeometry args={[0.9, 1, 3, 1]} />
+                        <meshStandardMaterial color="white" roughness={0.75} />
+                    </mesh>
+                </> : <>
+                    <mesh scale={ringScale} position={[3, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+                        <ringGeometry args={[0.9, 1, 4, 1]} />
+                        <meshStandardMaterial color="white" roughness={0.75} />
+                    </mesh>
+                    <mesh scale={ringScale} position={[-3, 0, 0.2]} rotation={[-Math.PI / 2, 0, Math.PI / 2.0]}>
+                        <ringGeometry args={[0.9, 1, 3, 1]} />
+                        <meshStandardMaterial color="white" roughness={0.75} />
+                    </mesh></>
+            }
+
             <Environment resolution={512}>
                 {/* Ceiling */}
                 <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -9]} scale={[10, 1, 1]} />
